f423cd2ae9a0ae9a7056581192be6f6f92fe2fc9
zeXc33d <a.elistratov@protonmail.com>
2016-11-10 19:58:58 +0300

    learning canvas/drawing a line

---
+++ javascript/lineCanvas/_scripts/line.js
@@ -0,0 +1,12 @@
+//line.js
+//10 Nov, 2016
+//github : https://github.com/PrizmIzBack
+window.onload = function(){
+
+    var canvas = document.querySelector('canvas');
+    var ctx = canvas.getContext('2d');
+    ctx.moveTo(50,100);
+    ctx.lineTo(100,60);
+    ctx.stroke();
+    
+}
